0d0c45e61a9f2413f5b648155e738f9618a1e85b
Dhilip KARTHIKEYAN <74380649+K-Dhilip@users.noreply.github.com>
2022-03-25 22:44:25 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let i = 0;
+
+function circle() {
+  if ((i = 0)) {
+    document.getElementById("box2").innerHTML = "I Am A Circle";
+  } else {
+    document.getElementById("box2").style.display = "none";
+  }
+}
